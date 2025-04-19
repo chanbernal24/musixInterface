6b61e51423ab25023b2181d5cba08f523985e198
Christian Bernal <bernal.johnchristian.08242003@gmail.com>
2025-04-19 18:17:10 +0800

    keyboard listener feature currently working

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from MusicService import MusicService
+import pygame
+import keyboard
+
 
 musicService = MusicService()
-#
+
 # songUrl = input("Enter Song URL: ")
 #
 # musicService.addSong(songUrl)
-
-musicService.listAllSongs()
+#
 
 songIndex = int(input("\nEnter Song Index to play: "))
 
 musicService.playSongWithLyrics(songIndex)
+if keyboard.is_pressed('q'):
+    pygame.mixer.quit()
+
 
 
 
